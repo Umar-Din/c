3ab23974c168a8daa5bc48e7f07bfb6dc4096b77
Umar Din Khan <umardinkhan2001@gmail.com>
2025-08-17 23:40:35 +0530

    20250817 First C Program Changes to be committed:       new file:   first_c_program.c

---
+++ first_c_program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	printf("Hello from C\n");
+
+	int anyIntergerNumber = 0;
+	printf("Please Enter Any Number\n");
+	scanf("%d", &anyIntergerNumber);
+	printf("Your Entered Number is > %d\n",anyIntergerNumber);
+	return 0;
+}
